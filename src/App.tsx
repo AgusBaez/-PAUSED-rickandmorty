f286cd5e81b0f6aa0e516af2d11cf188721203b1
Agus Baez <agustinbaezignacio@gmail.com>
2024-08-24 02:20:46 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>World of Rick & Morty</h1>
+      <div className="card">
+      </div>
+    </>
+  )
+}
+
+export default App
